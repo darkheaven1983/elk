afd5a50567c4dfb23139f3fa7850b75c69f68eff
darkheaven1983 <darkheaven1983@gmail.com>
2015-06-24 14:45:31 +0800

    update elastic host

--- kibana/run.sh
+++ kibana/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed -i -e "s/elasticsearch:9200/elasticsearch:${ELASTICSEARCH_PORT_9200_TCP_PORT}/g" /opt/kibana/config/kibana.yml 
+sed -i -e "s/elasticsearch:9200/${ELASTICSEARCH_PORT_9200_TCP_ADDR}:${ELASTICSEARCH_PORT_9200_TCP_PORT}/g" /opt/kibana/config/kibana.yml 
 /opt/kibana/bin/kibana
--- logstash/run.sh
+++ logstash/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-sed -i -e "s/9200/${ELASTICSEARCH_PORT_9200_TCP_PORT}/g" /etc/logstash/conf.d/logstash.conf
+sed -i -e "s/ELASTICSEARCH_PORT_9200_TCP_ADDR/${ELASTICSEARCH_PORT_9200_TCP_ADDR}/g" /etc/logstash/conf.d/logstash.conf
+sed -i -e "s/ELASTICSEARCH_PORT_9200_TCP_PORT/${ELASTICSEARCH_PORT_9200_TCP_PORT}/g" /etc/logstash/conf.d/logstash.conf
 /opt/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf
