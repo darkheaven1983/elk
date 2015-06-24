03beda9a06109410a536eaed4790b8c6858fa166
darkheaven1983 <darkheaven1983@gmail.com>
2015-06-24 16:34:06 +0800

    update elastic config

--- logstash/run.sh
+++ logstash/run.sh
@@ -2,4 +2,6 @@
 
 sed -i -e "s/ELASTICSEARCH_PORT_9200_TCP_ADDR/${ELASTICSEARCH_PORT_9200_TCP_ADDR}/g" /etc/logstash/conf.d/logstash.conf
 sed -i -e "s/ELASTICSEARCH_PORT_9200_TCP_PORT/${ELASTICSEARCH_PORT_9200_TCP_PORT}/g" /etc/logstash/conf.d/logstash.conf
+sed -i -e "s/ELASTICSEARCH_PORT_9200_TCP_ADDR/${ELASTICSEARCH_PORT_9200_TCP_ADDR}/g" /etc/logstash/elasticsearch.yml 
+
 /opt/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf
